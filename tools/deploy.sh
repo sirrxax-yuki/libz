1fecdab26b030f523def4772d7a5826b6a79ff4b
yuki.kawata <yuki.kawata@jp.ricoh.com>
2024-01-30 10:10:22 +0900

    bugfix deploy script.

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,12 +4,11 @@ AWS_REGISTRY=$(aws ssm get-parameter --query "Parameter.Value" --output text --n
 AWS_DATABASE=$(aws ssm get-parameter --query "Parameter.Value" --output text --name /libz/db/endpoint)
 AWS_DATABASE_USER=$(aws ssm get-parameter --query "Parameter.Value" --output text --name /libz/db/user)
 AWS_DATABASE_PASSWORD=$(aws ssm get-parameter --query "Parameter.Value" --output text --name /libz/db/password --with-decryption)
-IMAGE_NAME=${AWS_REGISTRY_NAME}/libz:latest
 
 docker rm -f libz-app 2>/dev/null
 
 aws ecr get-login-password | docker login --username AWS --password-stdin ${AWS_REGISTRY}
-docker pull ${IMAGE_NAME}
+docker pull ${Build.IMAGE_TAG}
 
 docker run \
     --name libz-app \
@@ -19,4 +18,4 @@ docker run \
     -e DATABASE_USER=${AWS_DATABASE_USER} \
     -e DATABASE_PASSWORD=${AWS_DATABASE_PASSWORD} \
     -e DATABASE_NAME=libz \
-    -d ${IMAGE_NAME}
+    -d ${Build.IMAGE_TAG}
