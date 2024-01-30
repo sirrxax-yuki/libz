2ae147d0283d999942a6c00e81651ff0e78837af
yuki.kawata <yuki.kawata@jp.ricoh.com>
2024-01-30 14:33:43 +0900

    bugfix deploy script.

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,13 +4,14 @@ AWS_REGISTRY=$(aws ssm get-parameter --query "Parameter.Value" --output text --n
 AWS_DATABASE=$(aws ssm get-parameter --query "Parameter.Value" --output text --name /libz/db/endpoint)
 AWS_DATABASE_USER=$(aws ssm get-parameter --query "Parameter.Value" --output text --name /libz/db/user)
 AWS_DATABASE_PASSWORD=$(aws ssm get-parameter --query "Parameter.Value" --output text --name /libz/db/password --with-decryption)
-IMAGE_TAG=$(cat artifacts.json | jq -r '.imageTag')
+IMAGE_TAG=$(cat /app/artifacts.json | jq -r '.imageTag')
 
 docker rm -f libz-app 2>/dev/null
 
 aws ecr get-login-password | docker login --username AWS --password-stdin ${AWS_REGISTRY}
 
 which jq
+ls -al
 echo ${IMAGE_TAG}
 
 docker pull ${IMAGE_TAG}
